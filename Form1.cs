fb562d51ae626b6e12ead70d684ee4ab8e82fe6b
Forni <and-not@mail.ru>
2021-03-01 21:48:25 +0300

    + gitignors3

--- Form1.cs
+++ Form1.cs
@@ -342,7 +342,7 @@ namespace SerchAndNotDestroy
         
         private void TestButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Привет, теперь этот код есть в 'git'!\nНа этом совсем  Все!");
+            MessageBox.Show("Привет, теперь этот код есть в 'git'!\nНа этом совсем ghjcnj e;fc Все!");
 
             /*Thread.Sleep(3000);
             srPer.ScreenShotActiveWindow();
